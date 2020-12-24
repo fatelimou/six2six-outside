46dd39056f48e9b8103c48d9a30e55488d57b60f
limozhi <wx_8ca1ca856d974a0b88f5308a4320e186@git.code.tencent.com>
2020-12-24 14:56:18 +0800

    --story=000000 构建项目

---
+++ outside-parent/outside-admin/src/main/java/cn/six2six/outside/admin/config/security/SecurityConfig.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright(c)) 2014-2019 Wegooooo Ltd. All rights reserved.
+ * <p>
+ * You may not use this file except authorized by Wegooooo.
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed is prohibited.
+ */
+package cn.six2six.outside.admin.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * spring-security配置
+ *
+ * @author limozhi on 2020/12/24
+ */
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //super.configure(http);
+        //配置不需要登陆验证
+        http.authorizeRequests().anyRequest().permitAll().and().logout().permitAll();
+    }
+
+}
