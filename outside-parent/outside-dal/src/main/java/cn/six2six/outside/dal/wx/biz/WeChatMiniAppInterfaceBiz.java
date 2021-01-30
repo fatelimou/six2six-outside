b62905003a4683c1a1bb1644914c820166e723c4
limozhi <wx_8ca1ca856d974a0b88f5308a4320e186@git.code.tencent.com>
2021-01-30 11:44:22 +0800

    --打包相关配置

--- outside-parent/outside-dal/src/main/java/cn/six2six/outside/dal/wx/biz/WeChatMiniAppInterfaceBiz.java
+++ outside-parent/outside-dal/src/main/java/cn/six2six/outside/dal/wx/biz/WeChatMiniAppInterfaceBiz.java
@@ -33,10 +33,7 @@ public class WeChatMiniAppInterfaceBiz {
      */
     public String loginCheck(String code){
         String url = "https://api.weixin.qq.com/sns/jscode2session?appid=APPID&secret=SECRET&js_code=JSCODE&grant_type=authorization_code";
-        url.replace("APPID",appId);
-        url.replace("SECRET",secret);
-        url.replace("JSCODE",code);
-
+        url = url.replaceAll("APPID",appId).replaceAll("SECRET",secret).replaceAll("JSCODE",code);
         String res = HttpUtils.get(url, null, r -> {
             return r;
         });
